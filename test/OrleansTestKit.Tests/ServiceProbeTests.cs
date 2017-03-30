aeda3c69a04d3a4746b68329a8b1d34e5d543378
Bohdan Sachkovskyi <bogdan.sachkovskyi@gmail.com>
2017-03-30 12:50:21 +0300

    add service probes. now it is possible to create grains with service dependencies. implement 'TestServiceProvider', use it with 'GrainCreator'.

---
+++ test/OrleansTestKit.Tests/ServiceProbeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using TestGrains;
+using TestInterfaces;
+using Xunit;
+
+namespace Orleans.TestKit.Tests
+{
+    public class ServiceProbeTests : TestKitBase
+    {
+        [Fact]
+        public async Task SayHelloTestShouldPrintDate()
+        {
+            // Arrange
+            const string greeting = "Bonjour";
+            var date = DateTime.UtcNow.Date;
+
+            var dateServiceMock = Silo.AddServiceProbe<IDateTimeService>();
+            dateServiceMock.Setup(i => i.GetCurrentDate())
+                    .ReturnsAsync(() => date);
+
+            var grain = Silo.CreateGrain<HelloGrainWithServiceDependency>(10);
+
+            // Act
+            var reply = await grain.SayHello(greeting);
+
+            // Assert
+            Assert.NotNull(reply);
+            Assert.Equal($"[{date}]: You said: '{greeting}', I say: Hello!", reply);
+
+            dateServiceMock.Verify(i => i.GetCurrentDate(), Times.Once);
+        }
+
+        [Fact]
+        public async Task SayHelloTestShouldPrintDefaultDateWhenServiceProbeNotDefined()
+        {
+            // Arrange
+            const string greeting = "Bonjour";
+
+            var grain = Silo.CreateGrain<HelloGrainWithServiceDependency>(10);
+
+            // Act
+            var reply = await grain.SayHello(greeting);
+
+            // Assert
+            Assert.NotNull(reply);
+            Assert.Equal($"[{default(DateTime).Date}]: You said: '{greeting}', I say: Hello!", reply);
+        }
+    }
+}
---
+++ test/TestInterfaces/IDateTimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestInterfaces
+{
+    public interface IDateTimeService
+    {
+        Task<DateTime> GetCurrentDate();
+    }
+}
