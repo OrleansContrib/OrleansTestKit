3b8843ce2ab22cfba027ea83a6e4adb1b6f2e4cd
Bohdan Sachkovskyi <bohdan.sachkovskyi@betlab.com>
2017-04-18 10:20:24 +0300

    uncomment test timer registry. add support for firing timer by index, in order of registration.

--- src/OrleansTestKit/Timers/TestTimer.cs
+++ src/OrleansTestKit/Timers/TestTimer.cs
@@ -1,25 +1,28 @@
-//namespace OrleansNonSiloTesting
-//{
-//    public class TestTimer : IDisposable
-//    {
-//        private Func<object, Task> _asyncCallback;
-//        private object _state;
-//
-//        public TestTimer(Func<object, Task> asyncCallback, object state)
-//        {
-//            _asyncCallback = asyncCallback;
-//            _state = state;
-//        }
-//
-//        public void Fire()
-//        {
-//            _asyncCallback?.Invoke(_state);
-//        }
-//
-//        public void Dispose()
-//        {
-//            _asyncCallback = null;
-//            _state = null;
-//        }
-//    }
-//}
+using System;
+using System.Threading.Tasks;
+
+namespace Orleans.TestKit.Timers
+{
+    public class TestTimer : IDisposable
+    {
+        private Func<object, Task> _asyncCallback;
+        private object _state;
+
+        public TestTimer(Func<object, Task> asyncCallback, object state)
+        {
+            _asyncCallback = asyncCallback;
+            _state = state;
+        }
+
+        public void Fire()
+        {
+            _asyncCallback?.Invoke(_state);
+        }
+
+        public void Dispose()
+        {
+            _asyncCallback = null;
+            _state = null;
+        }
+    }
+}
---
+++ test/OrleansTestKit.Tests/TimerTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using TestGrains;
+using Xunit;
+
+namespace Orleans.TestKit.Tests
+{
+    public class TimerTests : TestKitBase
+    {
+        [Fact]
+        public void ShouldFireAllTimers()
+        {
+            // Arrange
+            var grain = Silo.CreateGrain<HelloTimers>(0);
+
+            // Act
+            Silo.FireAllTimers();
+
+            // Assert
+            var state = Silo.State<HelloTimersState>(grain);
+            state.Timer0Fired.Should().BeTrue();
+            state.Timer1Fired.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldFireFirstTimer()
+        {
+            // Arrange
+            var grain = Silo.CreateGrain<HelloTimers>(0);
+
+            // Act
+            Silo.FireTimer(0);
+
+            // Assert
+            var state = Silo.State<HelloTimersState>(grain);
+            state.Timer0Fired.Should().BeTrue();
+            state.Timer1Fired.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldFireSecondTimer()
+        {
+            // Arrange
+            var grain = Silo.CreateGrain<HelloTimers>(0);
+
+            // Act
+            Silo.FireTimer(1);
+
+            // Assert
+            var state = Silo.State<HelloTimersState>(grain);
+            state.Timer0Fired.Should().BeFalse();
+            state.Timer1Fired.Should().BeTrue();
+        }
+    }
+}
