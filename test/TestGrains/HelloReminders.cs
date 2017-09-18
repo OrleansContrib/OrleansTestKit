9ca33366a5a628b20b449e6f93d4c84f70ba28c5
Daniel <danielrsarfati@gmail.com>
2017-09-18 10:19:50 -0700

    Added TestReminder Registry (#15)
    Mimic of Timer interface, but with reminders.

---
+++ test/TestGrains/HelloReminders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime;
+
+namespace TestGrains
+{
+    public class HelloReminders : Grain, IGrainWithIntegerKey, IRemindable
+    {
+        public List<string> FiredReminders = new List<string>();
+
+        public Task RegisterReminder(string reminderName,TimeSpan dueTime, TimeSpan period) =>
+            this.RegisterOrUpdateReminder(reminderName, dueTime, period);
+
+        async Task IRemindable.ReceiveReminder(string reminderName, TickStatus status)
+        {
+            FiredReminders.Add(reminderName);
+        }
+
+        public async Task UnregisterReminder(string reminderName)
+        {
+            var reminder = await this.GetReminder(reminderName);
+
+            await this.UnregisterReminder(reminder);
+        }
+    }
+}
