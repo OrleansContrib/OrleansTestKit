b9a75c4b55020d1286815b42fc1fbac6e0f65dc9
Eric Glanz <eric@ericglanz.com>
2017-11-14 09:20:16 -0700

    added optional class prefix support to TestGrainFactory (#24)

---
+++ test/TestGrains/DeviceManagerGrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+using TestInterfaces;
+
+namespace TestGrains
+{
+    public class DeviceManagerGrain : Grain, IDeviceManager
+    {
+        public Task<IDevice> GetDeviceGrain(string deviceType)
+        {
+            switch (deviceType)
+            {
+                case "IOS":
+                    return Task.FromResult(GrainFactory.GetGrain<IDevice>(deviceType, "TestGrains.DeviceIosGrain"));
+                case "Android":
+                    return Task.FromResult(GrainFactory.GetGrain<IDevice>(deviceType, "TestGrains.DeviceAndroidGrain"));
+                default:
+                    throw new Exception($"Unknown device type {deviceType}");
+            }
+        }
+    }
+}
---
+++ test/TestInterfaces/IDeviceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace TestInterfaces
+{
+    public interface IDeviceManager : IGrainWithIntegerKey
+    {
+        Task<IDevice> GetDeviceGrain(string deviceType);
+    }
+}
