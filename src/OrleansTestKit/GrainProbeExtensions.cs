8560696cc4ef40d169791bea8db0d74c4cceef27
Daniel Sarfati <danielrsarfati@gmail.com>
2017-12-19 21:57:35 -0800

    Cleaned up grain probe api to ensure the key type matches the grain type (#33)

--- src/OrleansTestKit/GrainProbeExtensions.cs
+++ src/OrleansTestKit/GrainProbeExtensions.cs
@@ -6,21 +6,16 @@ namespace Orleans.TestKit
 {
     public static class GrainProbeExtensions
     {
-        #region Probes
-
-        public static Mock<T> AddProbe<T>(this TestKitSilo silo, long id, string classPrefix = null) where T : class, IGrain
+        public static Mock<T> AddProbe<T>(this TestKitSilo silo, long id, string classPrefix = null) where T : class, IGrainWithIntegerKey
             => silo.GrainFactory.AddProbe<T>(new TestGrainIdentity(id), classPrefix);
 
-        public static Mock<T> AddProbe<T>(this TestKitSilo silo, Guid id, string classPrefix = null) where T : class, IGrain
+        public static Mock<T> AddProbe<T>(this TestKitSilo silo, Guid id, string classPrefix = null) where T : class, IGrainWithGuidKey
             => silo.GrainFactory.AddProbe<T>(new TestGrainIdentity(id), classPrefix);
 
-        public static Mock<T> AddProbe<T>(this TestKitSilo silo, string id, string classPrefix = null) where T : class, IGrain
+        public static Mock<T> AddProbe<T>(this TestKitSilo silo, string id, string classPrefix = null) where T : class, IGrainWithStringKey
             => silo.GrainFactory.AddProbe<T>(new TestGrainIdentity(id), classPrefix);
 
         public static void AddProbe<T>(this TestKitSilo silo, Func<IGrainIdentity, IMock<T>> factory) where T : class, IGrain
             => silo.GrainFactory.AddProbe<T>(factory);
-            
-
-        #endregion Probes
     }
 }
