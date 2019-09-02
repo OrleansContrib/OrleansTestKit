e2e95284a8905392bfa7f5a929d77a8d988382b9
Ilya Chernoudov <Ilya.Chernoudov@broadridge.com>
2019-09-02 15:25:24 +0300

    Added FireAsync for timers. #75

--- test/OrleansTestKit.Tests/Tests/TimerTests.cs
+++ test/OrleansTestKit.Tests/Tests/TimerTests.cs
@@ -53,5 +53,52 @@ namespace Orleans.TestKit.Tests
             state.Timer0Fired.Should().BeFalse();
             state.Timer1Fired.Should().BeTrue();
         }
+
+        [Fact]
+        public async Task ShouldFireAllTimersAsync()
+        {
+            // Arrange
+            var grain = await Silo.CreateGrainAsync<HelloTimers>(0);
+
+            // Act
+            await Silo.FireAllTimersAsync();
+
+            // Assert
+            var state = Silo.State<HelloTimersState>();
+            state.Timer0Fired.Should().BeTrue();
+            state.Timer1Fired.Should().BeTrue();
+        }
+
+
+
+        [Fact]
+        public async Task ShouldFireFirstTimerAsync()
+        {
+            // Arrange
+            var grain = await Silo.CreateGrainAsync<HelloTimers>(0);
+
+            // Act
+            await Silo.FireTimerAsync(0);
+
+            // Assert
+            var state = Silo.State<HelloTimersState>();
+            state.Timer0Fired.Should().BeTrue();
+            state.Timer1Fired.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ShouldFireSecondTimerAsync()
+        {
+            // Arrange
+            var grain = await Silo.CreateGrainAsync<HelloTimers>(0);
+
+            // Act
+            await Silo.FireTimerAsync(1);
+
+            // Assert
+            var state = Silo.State<HelloTimersState>();
+            state.Timer0Fired.Should().BeFalse();
+            state.Timer1Fired.Should().BeTrue();
+        }
     }
 }
