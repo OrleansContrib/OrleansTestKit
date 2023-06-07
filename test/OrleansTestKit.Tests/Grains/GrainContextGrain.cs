b347c8f8c62092c98316c55e199b34f85638a7b7
Carl Meyertons <cmeyertons@servicetitan.com>
2023-06-07 12:45:51 -0500

    adding grain context tests

---
+++ test/OrleansTestKit.Tests/Grains/GrainContextGrain.cs
@@ -0,0 +1,32 @@
+﻿using Orleans.Runtime;
+
+namespace Orleans.TestKit.Tests.Grains;
+internal class GrainContextGrain : Grain, IGrainWithGuidKey
+{
+    public IGrainContext Context { get; set; }
+
+    public GrainContextGrain(IGrainContext context)
+    {
+        Context = context;
+    }
+}
+
+internal sealed class GrainContextGrain2 : GrainContextGrain
+{
+    public GrainContextGrain2(IGrainContext context) : base(context) { }
+}
+
+internal sealed class GrainContextIntegerGrain : Grain, IGrainWithIntegerKey
+{ }
+
+internal sealed class GrainContextStringGrain : Grain, IGrainWithStringKey
+{ }
+
+internal sealed class GrainContextGuidGrain : Grain, IGrainWithGuidKey
+{ }
+
+internal sealed class GrainContextGuidCompoundGrain : Grain, IGrainWithGuidCompoundKey
+{ }
+
+internal sealed class GrainContextIntegerCompoundGrain : Grain, IGrainWithIntegerCompoundKey
+{ }
