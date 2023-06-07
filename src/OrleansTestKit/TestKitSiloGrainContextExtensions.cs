1bfbab6713e3e24af415ee71764b54fdcf7359d0
Carl Meyertons <cmeyertons@servicetitan.com>
2023-06-07 12:10:02 -0500

    GetOrAddGrainContext support

---
+++ src/OrleansTestKit/TestKitSiloGrainContextExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Orleans.Runtime;
+
+namespace Orleans.TestKit;
+/// <summary>
+/// Extension methods for creating grains from strongly typed IDs
+/// </summary>
+public static class TestKitSiloGrainContextExtensions
+{
+    /// <summary>
+    /// Create a grain - will be injected with a grain context and participate in grain lifecycle
+    /// </summary>
+    /// <typeparam name="T">The grain type (class not interface)</typeparam>
+    /// <param name="silo">The test kit silo</param>
+    /// <param name="id">The grain's long id</param>
+    /// <returns>The grain</returns>
+    public static IGrainContext GetOrAddGrainContext<T>(this TestKitSilo silo, long id)
+        where T : Grain, IGrainWithIntegerKey =>
+        silo.GetOrAddGrainContext<T>(GrainIdKeyExtensions.CreateIntegerKey(id));
+
+    /// <summary>
+    /// Create a grain - will be injected with a grain context and participate in grain lifecycle
+    /// </summary>
+    /// <typeparam name="T">The grain type (class not interface)</typeparam>
+    /// <param name="silo">The test kit silo</param>
+    /// <param name="id">The grain's id</param>
+    /// <returns>The grain</returns>
+    public static IGrainContext GetOrAddGrainContext<T>(this TestKitSilo silo, Guid id)
+        where T : Grain, IGrainWithGuidKey =>
+        silo.GetOrAddGrainContext<T>(GrainIdKeyExtensions.CreateGuidKey(id));
+
+    /// <summary>
+    /// Create a grain - will be injected with a grain context and participate in grain lifecycle
+    /// </summary>
+    /// <typeparam name="T">The grain type (class not interface)</typeparam>
+    /// <param name="silo">The test kit silo</param>
+    /// <param name="id">The grain's id</param>
+    /// <returns>The grain</returns>
+    public static IGrainContext GetOrAddGrainContext<T>(this TestKitSilo silo, string id)
+        where T : Grain, IGrainWithStringKey
+        => silo.GetOrAddGrainContext<T>(IdSpan.Create(id));
+
+    /// <summary>
+    /// Create a grain - will be injected with a grain context and participate in grain lifecycle
+    /// </summary>
+    /// <typeparam name="T">The grain type (class not interface)</typeparam>
+    /// <param name="silo">The test kit silo</param>
+    /// <param name="id">The grain's id</param>
+    /// <param name="keyExtension">The key extension</param>
+    /// <returns>The grain</returns>
+    public static IGrainContext GetOrAddGrainContext<T>(this TestKitSilo silo, Guid id, string keyExtension)
+        where T : Grain, IGrainWithGuidCompoundKey
+        => silo.GetOrAddGrainContext<T>(GrainIdKeyExtensions.CreateGuidKey(id, keyExtension));
+
+    /// <summary>
+    /// Create a grain - will be injected with a grain context and participate in grain lifecycle
+    /// </summary>
+    /// <typeparam name="T">The grain type (class not interface)</typeparam>
+    /// <param name="silo">The test kit silo</param>
+    /// <param name="id">The grain's id</param>
+    /// <param name="keyExtension">The key extension</param>
+    /// <returns>The grain</returns>
+    public static IGrainContext GetOrAddGrainContext<T>(this TestKitSilo silo, long id, string keyExtension)
+        where T : Grain, IGrainWithIntegerCompoundKey
+        => silo.GetOrAddGrainContext<T>(GrainIdKeyExtensions.CreateIntegerKey(id, keyExtension));
+}
