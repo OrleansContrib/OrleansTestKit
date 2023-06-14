61c249c97a673a0662c3495567d65a0502ccf28f
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2023-06-14 16:04:40 +0100

    Add tests for GrainId.Type value being correctly calculated.

---
+++ test/OrleansTestKit.Tests/Tests/GrainIdTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Orleans.Runtime;
+using TestGrains;
+using Xunit;
+
+namespace Orleans.TestKit.Tests;
+
+public class GrainIdTests : TestKitBase
+{
+    [Fact]
+    public async Task CreatedGrainShouldHaveCorrectGrainType()
+    {
+        var grain = await Silo.CreateGrainAsync<HelloGrain>(0);
+        GrainId id = grain.GetGrainId();
+
+        id.Type.Should().Be(GrainType.Create("hello"));
+    }
+
+    [Fact]
+    public async Task CreatedGrainWithAliasShouldHaveCorrectGrainType()
+    {
+        var grain = await Silo.CreateGrainAsync<AliasGrain>(0);
+        GrainId id = grain.GetGrainId();
+
+        id.Type.Should().Be(GrainType.Create("special-alias"));
+    }
+}
