72003a7598d4659a7411bc9571a7a9dca3e50224
Jakub Konecki <jkonecki@gmail.com>
2024-04-18 21:17:02 +0100

    Upgrade Orleans to version 8.1.0 (#160) (#161)
    Co-authored-by: Jakub Konecki <jakub.konecki@halny.tech>

--- src/OrleansTestKit/Utilities/RuntimeContextManager.cs
+++ src/OrleansTestKit/Utilities/RuntimeContextManager.cs
@@ -5,28 +5,30 @@ namespace Orleans.TestKit.Reminders;
 
 internal sealed class RuntimeContextManager
 {
+    public delegate void SetExecutionContextAction(IGrainContext? newContext, out IGrainContext currentContext);
+
     private RuntimeContextManager()
     {
         var assembly = typeof(GrainId).Assembly;
         var contextType = assembly.GetType("Orleans.Runtime.RuntimeContext")!;
 
         SetExecutionContext = contextType
-            .GetMethod("SetExecutionContext", BindingFlags.NonPublic | BindingFlags.Static, new Type[] { typeof(IGrainContext) })!
-            .CreateDelegate<Action<IGrainContext?>>()
+            .GetMethod("SetExecutionContext", BindingFlags.NonPublic | BindingFlags.Static)!
+            .CreateDelegate<SetExecutionContextAction>()
         ;
     }
 
     public static RuntimeContextManager Instance { get; } = new RuntimeContextManager();
 
-    public Action<IGrainContext?> SetExecutionContext { get; }
+    public SetExecutionContextAction SetExecutionContext { get; }
 
     public static IDisposable StartExecutionContext(IGrainContext context) => new RuntimeContextScope(context);
 
-    public void ResetExecutionContext() => SetExecutionContext(null);
+    public void ResetExecutionContext() => SetExecutionContext(null, out _);
 
     private sealed class RuntimeContextScope : IDisposable
     {
-        public RuntimeContextScope(IGrainContext context) => Instance.SetExecutionContext(context);
+        public RuntimeContextScope(IGrainContext context) => Instance.SetExecutionContext(context, out _);
 
         public void Dispose() => Instance.ResetExecutionContext();
     }
