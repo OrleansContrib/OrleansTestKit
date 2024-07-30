e4bf9ee7bc600419b674396e9bd1dbd329b12741
Alex McCool <alex.mccool@gmail.com>
2024-07-30 16:16:55 -0500

    correct the double call to OnActivateAsync on IRemindable grains
    Test grain for tracking OnActivateAsync call count
    Test OnActivateAsync is only called once

---
+++ test/OrleansTestKit.Tests/Grains/ActivationCountWithReminder.cs
@@ -0,0 +1,38 @@
+﻿using Orleans.Runtime;
+using Orleans.Streams;
+
+namespace TestGrains;
+
+public sealed class ActivationCountWithReminder : Grain, IGrainWithIntegerKey, IRemindable
+{
+    private int _activationCount;
+
+    public override Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        _activationCount++;
+        return base.OnActivateAsync(cancellationToken);
+    }
+
+    public Task<int> GetActivationCount() => Task.FromResult(_activationCount);
+
+
+    Task IRemindable.ReceiveReminder(string reminderName, TickStatus status)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task RegisterReminder(string reminderName, TimeSpan dueTime, TimeSpan period)
+    {
+        return this.RegisterOrUpdateReminder(reminderName, dueTime, period);
+    }
+
+    public async Task UnregisterReminder(string reminderName)
+    {
+        var reminder = await this.GetReminder(reminderName);
+
+        if (reminder != null)
+        {
+            await this.UnregisterReminder(reminder);
+        }
+    }
+}
